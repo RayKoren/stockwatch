b2545c8b83d7ae5dfc04ea6cb10804f023e4306a
rayk216 <raykoren@gmail.com>
2016-08-11 15:16:25 -0600

    fixed port error

--- public/core.js
+++ public/core.js
@@ -9,7 +9,7 @@ function mainController($scope, $http) {
     $scope.formData = {};
 
     $scope.getQuote = function() {
-        $http.get("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + $scope.formData.text + "%22)&format=json&env=store://datatables.org/alltableswithkeys")
+        $http.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + $scope.formData.text + "%22)&format=json&env=store://datatables.org/alltableswithkeys")
             .success(function(data) {
                 console.log(data);
                 var stockPrice = data.query.results.quote.Ask;
